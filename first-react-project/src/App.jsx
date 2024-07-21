30e75e133651e47f9b73403003a5330731a1c57d
Fanel lag <dieudevant95@gmail.com>
2024-07-21 16:59:55 -0300

    FirtsProjetreact

---
+++ first-react-project/src/App.jsx
@@ -0,0 +1,29 @@
+export default function (){
+return (
+<div className="app"> 
+<img src="/react1.png" alt="logo"/>
+<h1> J'apprends le reat</h1>
+<p> React is amazing</p>
+
+<div>
+  <button>My Button</button>
+  <button>I love react</button>
+</div>
+
+<hr/>
+<h1>Firt Project React</h1>
+<h2>Do you luke react</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+  Quas nobis ut officia odit blanditiis, delectus doloribus
+   eaque pariatur amet numquam fugit excepturi rerum corrupti 
+   voluptatibus, quaerat maxime asperiores. Repellat, earum!</p>
+
+
+
+</div>
+
+
+)
+}
+
+
